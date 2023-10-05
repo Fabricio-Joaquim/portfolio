3d724caf799045adb190e0a03162fd786a3b95e1
Fabricio Joaquim <fabricio.melo@dcomp.ufs.br>
2023-10-05 15:45:29 -0300

    build: :bulb: update build project

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), react()],
+  plugins: [legacy(), react(), tsconfigPaths()],
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
