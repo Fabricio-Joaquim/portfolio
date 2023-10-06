9f348140db0af2dc535af59888301964cfcc843a
Fabricio Joaquim <fabricio.melo@dcomp.ufs.br>
2023-10-06 15:14:45 -0300

    chore: :lipstick: add react-icons lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import Logo from "@/assets/logo.png";
-import HelloWorld from "@/components/HelloWorld/HelloWorld";
+import { Header } from "@components/Header";
 
 import styles from "./App.module.css";
 
 export default function App() {
   return (
     <main className={styles.main}>
-      <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
+      <Header />
     </main>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto"],
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) alternate, blink .7s infinite"
+      }
+    },
+  },
+  plugins: [],
+}
+
